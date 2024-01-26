2074604817f30c7d10643b96b887c711d99e293c
Leandro Gonçalves Leite <leandro_goncalves_leite@yahoo.com.br>
2024-01-25 21:17:14 -0300

    Revert "deixando o jogo mais facil!"
    This reverts commit dbdb8099216b4a7d8929a5f15b85d5bb1a5586ac.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 50;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativa = 1;
 
@@ -11,7 +11,7 @@ function exibirTextoTela(tag, texto) {
 
 function exibirMensagemInical() {
     exibirTextoTela("h1", "O número secreto");
-    exibirTextoTela("p", "Escolha um numero entre 1 e 50");    
+    exibirTextoTela("p", "Escolha um numero entre 1 e 100");    
 }
 
 exibirMensagemInical()
